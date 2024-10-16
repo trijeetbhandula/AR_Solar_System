a0c2ce9c22beb74fdd5cfb2f2c31916f3fadb233
trijeetbhandula <trijeet.bhandula@my.jcu.edu.au>
2024-10-16 18:20:14 +1000

    Removing the plane screen with the video

--- app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
+++ app/src/main/java/com/xperiencelabs/arapp/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.xperiencelabs.arapp
 
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.isGone
@@ -10,16 +9,12 @@ import io.github.sceneview.ar.ArSceneView
 import io.github.sceneview.ar.node.ArModelNode
 import io.github.sceneview.ar.node.PlacementMode
 import io.github.sceneview.math.Position
-import io.github.sceneview.node.VideoNode
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var sceneView: ArSceneView
     lateinit var placeButton: ExtendedFloatingActionButton
     private lateinit var modelNode: ArModelNode
-    private lateinit var videoNode: VideoNode
-    private lateinit var mediaPlayer:MediaPlayer
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,53 +24,31 @@ class MainActivity : AppCompatActivity() {
             this.lightEstimationMode = Config.LightEstimationMode.DISABLED
         }
 
-        mediaPlayer = MediaPlayer.create(this,R.raw.ad)
-
         placeButton = findViewById(R.id.place)
 
         placeButton.setOnClickListener {
             placeModel()
         }
 
-        videoNode = VideoNode(sceneView.engine, scaleToUnits = 0.7f, centerOrigin = Position(y=-4f), glbFileLocation = "models/plane.glb", player = mediaPlayer, onLoaded = {_,_ ->
-            mediaPlayer.start()
-        })
-
-        modelNode = ArModelNode(sceneView.engine,PlacementMode.INSTANT).apply {
+        modelNode = ArModelNode(sceneView.engine, PlacementMode.INSTANT).apply {
             loadModelGlbAsync(
                 glbFileLocation = "models/solar_system.glb",
                 scaleToUnits = 1f,
                 centerOrigin = Position(-0.5f)
-
-            )
-            {
+            ) {
                 sceneView.planeRenderer.isVisible = true
                 val materialInstance = it.materialInstances[0]
             }
             onAnchorChanged = {
                 placeButton.isGone = it != null
             }
-
         }
+
         sceneView.addChild(modelNode)
-        modelNode.addChild(videoNode)
-
     }
 
-   private fun placeModel(){
-       modelNode.anchor()
-
-       sceneView.planeRenderer.isVisible = false
-
-   }
-
-    override fun onPause() {
-        super.onPause()
-        mediaPlayer.stop()
+    private fun placeModel() {
+        modelNode.anchor()
+        sceneView.planeRenderer.isVisible = false
     }
-    override fun onDestroy() {
-        super.onDestroy()
-        mediaPlayer.release()
-    }
-
 }
